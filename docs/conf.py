69f4bb8150b0dcecc5ae14c444d3ba5cd52c9cf3
kareem1925 <kareemessafty@gmail.com>
2024-10-01 13:05:01 +0200

    modify autodoc_import

--- docs/conf.py
+++ docs/conf.py
@@ -9,32 +9,31 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
-project = 'photon_weave'
-copyright = '2024, Simon Sekavčnik, Kareem H. El-Safty, Janis Nötzel'
-author = 'Simon Sekavčnik, Kareem H. El-Safty, Janis Nötzel'
-release = '0.0.3'
+project = "photon_weave"
+copyright = "2024, Simon Sekavčnik, Kareem H. El-Safty, Janis Nötzel"
+author = "Simon Sekavčnik, Kareem H. El-Safty, Janis Nötzel"
+release = "0.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',  # for Google-style and NumPy-style docstrings
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',  # links to the source code
-    'sphinx.ext.autosummary',  # summary tables for documentation
+    "sphinx.ext.napoleon",  # for Google-style and NumPy-style docstrings
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",  # links to the source code
+    "sphinx.ext.autosummary",  # summary tables for documentation
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-autodoc_mock_imports = ["jax", "scipy"]
+autodoc_mock_imports = ["jax", "scipy", "numpy"]
 
 html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
