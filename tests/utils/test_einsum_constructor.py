538a01884aae1314b3516dd036d16243156d8347
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2025-03-19 11:04:53 +0100

    Fixed and tested einsum generation for trace_out

---
+++ tests/utils/test_einsum_constructor.py
@@ -0,0 +1,15 @@
+import unittest
+
+from photon_weave.state.fock import Fock
+import photon_weave.extra.einsum_constructor as ESC
+
+class TestTraceOut(unittest.TestCase):
+    def test_trace_out_matrix(self) -> None:
+        """
+        Test the correctness of the trace out einsum string generation
+        """
+        fock1 = Fock()
+        fock2 = Fock()
+        fock3 = Fock()
+        einsum = ESC.trace_out_matrix([fock1, fock2, fock3], [fock2])
+        self.assertEqual(einsum, "abcadc->bd")
